d8beb7ae6091768dd88cdf38d8894b8238a531f0
Buddhika Rathnayake <rathnayakearmb@gmail.com>
2026-05-02 21:20:22 +0530

    server created

---
+++ index.js
@@ -0,0 +1,29 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import bodyParser from "body-parser";
+
+
+const app = express();
+
+app.use(bodyParser.json());
+
+dotenv.config();
+
+const PORT = process.env.PORT || 5000;
+
+const MONGOURL = process.env.MONGO_URL;
+
+mongoose.connect(MONGOURL)
+    .then(() => {
+        console.log("Connected successfully")
+        
+        app.listen(PORT,()=>{
+        console.log(`Server is running on port : ${PORT}`)
+    });
+
+})
+.catch((error) => console.log(error));
+
+app.use("/api/user",route);
+
