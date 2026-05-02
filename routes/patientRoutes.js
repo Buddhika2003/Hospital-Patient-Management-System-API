98940ac276e9dbbe9572a435749cc99677ff9150
Buddhika Rathnayake <rathnayakearmb@gmail.com>
2026-05-02 22:13:34 +0530

    update patient route

--- routes/patientRoutes.js
+++ routes/patientRoutes.js
@@ -1,11 +1,10 @@
 import express from "express";
-import {createPatient,getAllPatients,updatePatient,deletePatient} from "../controllers/patientControllers.js";
+import {createPatient,getAllPatients,deletePatient} from "../controllers/patientControllers.js";
 
 const router = express.Router();
 
 router.get("/getAll", getAllPatients);
 router.post("/create", createPatient);
-router.put("/update/:id", updatePatient);
 router.delete("/delete/:id", deletePatient);
 
 export default router;
